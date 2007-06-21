da4d3dfbb892eee426292a8064fa555b2f784d7a
Tikkanz <tikkanz@gmail.com>
2007-06-21 01:55:32 +0000

    Complex user database
    git-svn-id: https://free2.projectlocker.com/RikkiTikkiTavi/selectj/svn/trunk@12 9a28d70f-7318-3d49-a9e1-554383617220

---
+++ Website/code/select_cmplx_data.sql
@@ -0,0 +1,65 @@
+-- This script populates the database with dummy data
+-- Populating tables
+
+begin transaction;
+
+insert into institutions (in_name, in_code, in_contact) values ('Massey University', 'MU', 1);
+insert into institutions (in_name, in_code, in_contact) values ('Lincoln University', 'LU', 4);
+insert into institutions (in_name, in_code, in_contact) values ('AgResearch', 'AGR', 2);
+
+insert into delivmodes (dm_name, dm_code, dm_descr) values ('Internal', 'INT', 'Course delivered on-campus.');
+insert into delivmodes (dm_name, dm_code, dm_descr) values ('Extramural', 'XM', 'Course delivered by correspondence.');
+insert into delivmodes (dm_name, dm_code, dm_descr) values ('Block', 'BLK', 'Course delivered on-campus in blocks.');
+
+insert into semesters (sm_name, sm_code, sm_descr) values ('Semester One', '01', 'February through June');
+insert into semesters (sm_name, sm_code, sm_descr) values ('Semester Two', '02', 'July through November');
+insert into semesters (sm_name, sm_code, sm_descr) values ('Semester Three', '03', 'November through February');
+insert into semesters (sm_name, sm_code, sm_descr) values ('Double Semester', '12', 'February through November');
+
+insert into roles (rl_name, rl_code, rl_descr) values ('student', 'STUD', 'Student in the course');
+insert into roles (rl_name, rl_code, rl_descr) values ('Template Designer', 'TDESIGN', 'User can create and edit templates');
+insert into roles (rl_name, rl_code, rl_descr) values ('Course Administrator', 'CADMIN', 'Person responsible for administering the course');
+insert into roles (rl_name, rl_code, rl_descr) values ('Site Administrator', 'SADMIN', 'Super user');
+
+insert into people (pp_fname, pp_lname, pp_email) values ('Ric', 'Sherlock', 'r.g.sherlock@massey.ac.nz');
+insert into people (pp_fname, pp_lname, pp_email) values ('Tricia', 'Johnson', 'tricia.johnson@hotmail.co.nz');
+insert into people (pp_fname, pp_lname, pp_email) values ('Jason', 'Win', 'onfire@xtra.co.nz');
+insert into people (pp_fname, pp_lname, pp_email) values ('Joe', 'Bloggs', 'jbloggs@xtra.co.nz');
+
+insert into scendefs (sd_filen) values ('BiggerPopln/animalsim.ini');
+insert into scendefs (sd_filen) values ('ChangeREVs/animalsim.ini');
+insert into scendefs (sd_filen) values ('SelectManual/animalsim.ini');
+
+insert into users (ur_ppid, ur_inid, ur_uname, ur_passhash, ur_salt) values (1,1,'tikka','55f9078cf55595a928ec8cf35e20d106',2079797903);
+insert into users (ur_ppid, ur_inid, ur_uname, ur_passhash, ur_salt) values (3,1,'injapan','6530575557447b07e16da4204329c8a8',-865438670);
+insert into users (ur_ppid, ur_inid, ur_uname, ur_passhash, ur_salt) values (2,3,'tricia','ef5c0caabb090e216e24adfeca4ca316',64988700);
+insert into users (ur_ppid, ur_inid, ur_uname, ur_passhash, ur_salt) values (4,2,'bloggs','8f90eacdc8ffe4e71b52906865d81f4c',-228865924);
+
+insert into courses (cr_name, cr_code, cr_descr, crinid) values ('Sheep Production','117.352','All about sheep production including genetics',1);
+insert into courses (cr_name, cr_code, cr_descr, crinid) values ('Animal Breeding and Genetics','227.202','Genetics for vets',1);
+insert into courses (cr_name, cr_code, cr_descr, crinid) values ('Genetics for Livestock Improvement','117.345','All about genetics',1);
+insert into courses (cr_name, cr_code, cr_descr, crinid) values ('Principles of Animal Production','117.254','Animal Production including genetics',1);
+insert into courses (cr_name, cr_code, cr_descr, crinid) values ('Experiment with AnimalSim','AnSim','Course for guests to play with AnimalSim',1);
+insert into courses (cr_name, cr_code, cr_descr, crinid) values ('Production Animal Science','ASC101','Intro course with genetics',2);
+insert into courses (cr_name, cr_code, cr_descr, crinid) values ('Using Genetics','USEGEN','Extension for farmers',3);
+
+insert into offerings (of_crid, of_year, of_smid, of_dmid) values (1,2007,1,1);
+insert into offerings (of_crid, of_year, of_smid, of_dmid) values (1,2007,1,1);
+insert into offerings (of_crid, of_year, of_smid, of_dmid) values (2,2007,2,1);
+insert into offerings (of_crid, of_year, of_smid, of_dmid) values (3,2007,4,2);
+insert into offerings (of_crid, of_year, of_smid, of_dmid) values (7,2007,2,3);
+insert into offerings (of_crid, of_year, of_smid, of_dmid) values (5,2007,4,2);
+insert into offerings (of_crid, of_year, of_smid, of_dmid) values (5,2008,4,2);
+insert into offerings (of_crid, of_year, of_smid, of_dmid) values (5,2009,4,2);
+insert into offerings (of_crid, of_year, of_smid, of_dmid) values (5,2010,4,2);
+
+insert into enrolments (en_urid, en_ofid, en_rlid) values ();
+
+insert into templates (tm_sdid, tm_opt1, tm_opt2, tm_opt3) values ();
+
+insert into templatetext (tt_tmid, tt_intro, tt_instruct, tt_btwncycle, tt_conc) values ();
+insert into templatetext (tt_tmid, tt_intro, tt_instruct, tt_conc) values ();
+
+insert into offeringtemplates (ot_ofid, ot_tmid) values ();
+
+commit;
---
+++ Website/code/select_cmplx_ddl.sql
@@ -0,0 +1,122 @@
+-- This script creates the tables for Select - the Web interface of AnimalSim
+
+-- Dropping tables users 
+
+--begin transaction;
+--drop table users;
+--drop table passrecvry;
+--commit;
+
+-- Create the new tables
+
+-- tables with no foreign keys
+CREATE TABLE institutions (
+  in_id    INTEGER NOT NULL PRIMARY KEY,
+  in_name  CHAR(50) NOT NULL,  -- full name for institution eg. Massey University 
+  in_code  CHAR(15) NOT NULL,  -- abbreviation eg. MU
+  in_descr CHAR(100) DEFAULT NULL, -- description of institution
+  in_addr  CHAR(200) DEFAULT NULL, -- institute postal address
+  in_contact INTEGER REFERENCES people(pp_id));  --reference contact person in people table
+
+CREATE TABLE delivmodes (
+  dm_id    INTEGER NOT NULL PRIMARY KEY,
+  dm_name  CHAR(30) NOT NULL,  -- full name for delivery mode eg. Extramural
+  dm_code  CHAR(10) NOT NULL,  -- abbreviation eg. XM
+  dm_descr CHAR(100) );        -- description of delivery mode eg. study by correspondence
+
+CREATE TABLE semesters (
+  sm_id    INTEGER NOT NULL PRIMARY KEY,
+  sm_name  CHAR(30) NOT NULL,  -- full name for semester eg. Double Semester
+  sm_code  CHAR(10) NOT NULL,  -- abbreviation eg. 1 or 12
+  sm_descr CHAR(100) );        -- description/definition of semester
+
+CREATE TABLE roles (
+  rl_id    INTEGER NOT NULL PRIMARY KEY,
+  rl_name  CHAR(30) NOT NULL,
+  rl_code  CHAR(10) NOT NULL,  -- abbreviation eg. usr or admin
+  rl_descr CHAR(100) );        -- description of roles
+  
+CREATE TABLE people (
+  pp_id   INTEGER NOT NULL PRIMARY KEY,
+  pp_fname  CHAR(30) NOT NULL,    -- first names
+  pp_lname  CHAR(30) NOT NULL,    -- last names
+  pp_pname  CHAR(30) DEFAULT NULL,-- preferred name
+  pp_initls CHAR(10) DEFAULT NULL,-- initials
+  pp_gendr  CHAR(10) DEFAULT NULL,-- gender
+  pp_dob    INTEGER  DEFAULT NULL,-- date of birth (julianday)
+  pp_email  CHAR(40) NOT NULL);   -- email here instead of user record for uniqueness? 
+
+CREATE TABLE scendefs (
+  sd_id    INTEGER NOT NULL PRIMARY KEY,
+  sd_filen CHAR(100) ); -- instead of file name could store files as BLOBS?
+  
+-- tables with foreign keys  
+CREATE TABLE users (
+	ur_id     INTEGER NOT NULL PRIMARY KEY,
+	ur_ppid   INTEGER NOT NULL REFERENCES people(pp_id),
+	ur_inid   INTEGER NOT NULL REFERENCES institutions(in_id),
+	ur_uname    CHAR(20)  UNIQUE NOT NULL, -- username 
+	ur_passhash CHAR(32),                  -- hashed (salt + password)
+	ur_salt     INTEGER,                   -- salt for creating hashed password
+	ur_status   INTEGER NOT NULL DEFAULT 1,--status active (1) or not (0)
+	ur_refnum   CHAR(10)  DEFAULT NULL);   --reference number -institution student/staff ID number
+  --	ur_email     CHAR(30)  NOT NULL);  --email address - store in people instead
+
+CREATE TABLE passrecvry (  -- table used to help users recover from forgotten password
+  ps_id     INTEGER NOT NULL PRIMARY KEY,
+  ps_urid   INTEGER NOT NULL,   -- uid to recover for
+  ps_token  CHAR(30),  -- forgotten password recovery token
+  ps_tstamp REAL);   -- timestamp for tokens (julianday.time) 
+
+CREATE TABLE courses (  -- courses offered by each institution
+  cr_id     INTEGER NOT NULL PRIMARY KEY,
+  cr_name   CHAR(50) NOT NULL,  -- course name
+  cr_code   CHAR(10) NOT NULL,  -- course number/code eg. '117.010' or 'ANS-110'
+  cr_descr  CHAR(700),          -- course description
+  cr_inid   INTEGER NOT NULL REFERENCES institutions(in_id) );
+
+CREATE TABLE offerings (  -- an offering of a course in terms of year, delivery mode and semester
+  of_id     INTEGER NOT NULL PRIMARY KEY,
+  of_crid   INTEGER NOT NULL REFERENCES courses(cr_id) ,
+  of_year   INTEGER NOT NULL DEFAULT NOW(),
+  of_smid   INTEGER NOT NULL REFERENCES semesters(sm_id),
+  of_dmid   INTEGER NOT NULL REFERENCES delivmodes(dm_id) );
+
+CREATE TABLE enrolments (  -- intersection of user, offering and user role for that offering
+  en_id     INTEGER NOT NULL PRIMARY KEY,
+  en_urid   REFERENCES users(ur_id),
+  en_ofid   REFERENCES offerings(of_id),
+  en_rlid   REFERENCES roles(rl_id));
+  
+CREATE TABLE sessions (   -- log of user sessions
+  ss_id     INTEGER NOT NULL PRIMARY KEY,
+  ss_start  REAL,  -- stored as julianday.time
+  ss_finish REAL,  -- stored as julianday.time
+  ss_enid   INTEGER NOT NULL REFERENCES enrolments(en_id)); -- session starts when choose offering not at login?
+
+CREATE TABLE templates (
+  tm_id    INTEGER NOT NULL PRIMARY KEY,
+  tm_sdid  INTEGER NOT NULL REFERENCES scendefs(sd_id),
+  tm_opt1  INTEGER NOT NULL DEFAULT 1,
+  tm_opt2  INTEGER NOT NULL DEFAULT 1,
+  tm_opt3  INTEGER NOT NULL DEFAULT 1); -- a bunch of boolean fields determining ScenDef options to include
+
+CREATE TABLE templatetext (  -- text that applicable to each scenario/template
+  tt_id        INTEGER NOT NULL PRIMARY KEY,
+  tt_tmid      REFERENCES templates(tm_id),
+  tt_intro     CHAR(1000),  -- intro text to set scene for scenario
+  tt_instruct  CHAR(1000),  -- instruction text for scenario
+  tt_btwncycle CHAR(1000),  -- text to display between selection cycles
+  tt_conc      CHAR(1000)); -- text to display at conclusion of selection cycles
+
+CREATE TABLE offeringtemplates (  -- templates available for each offering
+  ot_id   INTEGER NOT NULL PRIMARY KEY,
+  ot_ofid INTEGER NOT NULL REFERENCES offerings(of_id) ,
+  ot_tmid INTEGER NOT NULL );
+
+CREATE TABLE errors (
+  er_id     INTEGER NOT NULL PRIMARY KEY,
+  er_ssid   INTEGER NOT NULL REFERENCES sessions(ss_id),
+  er_tmid   INTEGER NOT NULL REFERENCES templates(tm_id),
+  er_sdid   INTEGER NOT NULL REFERENCES scendefs(sd_id),
+  er_errmsg CHAR(100) );  -- text of last error message   
